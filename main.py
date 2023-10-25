5e7e2b7774b16b17fce03dba1a9d83a8b99340ff
AOPeasare01 <90740168+AOPeasare01@users.noreply.github.com>
2023-10-25 02:30:33 +0000

    Getting pages authored and adding user data

--- main.py
+++ main.py
@@ -61,3 +61,41 @@ if __name__ == "__main__":  # Makes sure this is the main process
 		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
 		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
 	)
+
+def add_page_to_user_data(self,username,file_name,storage_client=storage.Client(),json_module=json):
+  ''' Updates user list of pages authored after uploading a page
+
+  Processes the user json file to a python dictionary, adds the new uploaded page file name to the list.
+  Processes the dictionary back to json and uploads.
+
+  Args:
+      username: used to update the right user file
+      file_name: used to add the file name to the user list of pages authored
+      storage_client: used to inject mock storage, uses google storage by default
+      json_module: used to inject mock json, uses normal json by default
+  '''
+  bucket = storage_client.bucket('ama_users_passwords')
+  blob = bucket.blob(username)
+  user_data = {}
+  with blob.open('r') as b:
+      user_data = json_module.loads(b.read())
+  user_data['pages_uploaded'].append('pages/'+file_name)
+  json_data = json_module.dumps(user_data)
+  blob.upload_from_string(json_data,content_type="application/json")
+
+def get_pages_authored(self,username,storage_client=storage.Client(),json_module=json):
+  '''Gets the list of pages authored by the user
+
+  Converts user data from json to python dictionary and returns key pages_uploaded that contains a list of pages authored
+
+  Args:
+      username: used to get the data from the user
+      storage_client: used to inject mock storage, uses google storage by default
+      json_module: used to inject mock json, uses normal json by default
+  '''
+  bucket = storage_client.bucket('ama_users_passwords')
+  blob = bucket.blob(username)
+  user_data = {}
+  with blob.open('r') as b:
+      user_data = json_module.loads(b.read())
+  return user_data['pages_uploaded']
